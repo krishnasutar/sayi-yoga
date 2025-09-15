09c6e16bcc7efd6b5aeab75a3c592fc50565fdd5
krishnasutar <krishnasuthar717@gmail.com>
2025-09-15 05:56:55 +0530

    update

---
+++ constant.php
@@ -0,0 +1,9 @@
+<?php
+defined('COMPANY_NAME')       OR define('COMPANY_NAME','Terravertex ');    
+defined('COMPANY_URL')       OR define('COMPANY_URL','https://www.terravertex.co.in/');
+defined('COMPANY_WURL')       OR define('COMPANY_WURL','www.terravertex.co.in/');
+
+// defined('WEBSITE_URL')       OR define('WEBSITE_URL','https://www.sayi.growthsphereindustries.com/');
+defined('WEBSITE_URL')       OR define('WEBSITE_URL','http://localhost/sayi-yoga/');
+
+?>
